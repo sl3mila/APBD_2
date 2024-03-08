22fda677002fd99f898c19e0748ade9c9f43dc1c
Emilia Ślemp <s28796@pjwstk.edu.pl>
2024-03-08 15:00:29 +0100

    mod GetAvg

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+int[] arr = new[] { 1, 2, 3, 4, 5 };
+
 Console.WriteLine("Hello,, World!!");;
+Console.WriteLine(GetAvg(arr));
+
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+
+    return (double)sum / arr.Length;
+}
